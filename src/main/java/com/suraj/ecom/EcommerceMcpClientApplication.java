07302fcc256e1b3169ab7e1850f2263166c295c1
surajbadhe <surajdbadhe@gmail.com>
2025-09-21 19:07:45 +0100

    First Commit. Project Structure

---
+++ src/main/java/com/suraj/ecom/EcommerceMcpClientApplication.java
@@ -0,0 +1,13 @@
+package com.suraj.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceMcpClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceMcpClientApplication.class, args);
+	}
+
+}
